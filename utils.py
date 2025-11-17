b53ef94e2b55724f4d26e1b7ba2062d866703a9b
daniel-onate <donatega@gmail.com>
2025-11-17 15:02:59 +0100

    Added early stopping class

--- utils.py
+++ utils.py
@@ -7,3 +7,22 @@ def dice_coeff(outputs, masks, smooth=1e-6):
     dice = (2. * intersection + smooth) / (outputs.sum() + masks.sum() + smooth)
 
     return dice.item()
+
+
+class EarlyStopping():
+    def __init__(self, patience, delta):
+        self.stop_training = False
+        self.patience = patience
+        self.delta = delta
+        self.counter = 0
+        self.best_loss = None
+
+    def check(self, val_loss):
+        if self.best_loss == None or val_loss < self.best_loss - self.delta:
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.stop_training = True
+                print('Early stopping')
